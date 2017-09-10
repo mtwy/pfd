a9be9cbe9a45bef37e89b7ba7c89cd1520d60c52
mtwy <836242689@qq.com>
2017-09-10 12:35:30 +0800

    1

---
+++ src/main/java/com/longhui/common/constant/SystemConstant.java
@@ -0,0 +1,14 @@
+package com.longhui.common.constant;
+
+public class SystemConstant {
+
+	// 服务端
+	public static final String ERROR_MESSAGE_SERVER_F01 = "服务端处理异常";
+	public static final String ERROR_MESSAGE_SERVER_CODE_F01 = "-1";
+
+	public static final String MESSAGE_SERVER_Z00 = "服务端处理正常";
+	public static final String MESSAGE_SERVER_CODE_Z00 = "0";
+
+	public static final String ERROR_MESSAGE_SERVER_F02 = "服务端返回结果解析错误";
+	public static final String ERROR_MESSAGE_SERVER_CODE_F02 = "-2";
+}
